bb0c307ba6915dda60fba5594d24eab7148f11fd
Karl Keat <karlkeat@gmail.com>
2016-08-08 16:50:41 -0400

    Add support for random gene duplications in the species tree

--- evolution_sim/tree_generator.py
+++ evolution_sim/tree_generator.py
@@ -40,13 +40,16 @@ def generate_perfect_tree(num_taxa, groups):  # Generates a perfect bipartition 
 
 class TreeGenerator:
     def __init__(self, num_genes, num_taxa, seed=12345, branch_length=1.0, branch_stdev=None, drop_chance=0.0,
-                 num_drops=0):
+                 num_drops=0, duplication_chance=0.0, num_duplications=0):
         self.num_genes = num_genes
         self.num_taxa = num_taxa
+        self.branch_length = branch_length
 
         self.rand_gen = Random(seed)  # Random seed for the hashing
         self.drop_chance = drop_chance
         self.num_drops = num_drops
+        self.duplication_chance = duplication_chance
+        self.num_duplications = num_duplications
 
         self.genes = list()  # Lists of hashes to prevent collisions
         self.taxa = list()
@@ -97,8 +100,17 @@ class TreeGenerator:
     def _copy_species_tree(self, gene):  # Returns a copy of the species tree with a unique gene name
         tree = copy.deepcopy(self.species_tree.clade)
         self._recursive_rename(tree, gene)
-        leaves = tree.get_terminals()
+
+        for x in range(self.num_duplications):
+            leaves = tree.get_terminals()
+            duplicate = self.rand_gen.random()
+            if duplicate <= self.duplication_chance:
+                duplicate = self.rand_gen.choice(leaves)
+                duplicate.split(branch_length=self.branch_length)
+                duplicate.name = ""
+
         for x in range(self.num_drops):
+            leaves = tree.get_terminals()
             drop = self.rand_gen.random()
             if drop <= self.drop_chance:
                 drop = self.rand_gen.choice(leaves)
@@ -132,9 +144,13 @@ def main():
                         action='store', type=float, default=None)
     parser.add_argument('-bs', '--branch_stdev', help='The standard deviation of the gene tree branch length',
                         action='store', type=float, default=None)
-    parser.add_argument('-dc', '--drop_chance', help='The probability of losing a species in each species tree',
+    parser.add_argument('-drp', '--drop_chance', help='The probability of losing a species in each species tree',
                         action='store', type=float, default=0)
-    parser.add_argument('-nd', '--num_drops', help='The number of times to try dropping a species from the tree',
+    parser.add_argument('-ndr', '--num_drops', help='The number of times to try dropping a species from the tree',
+                        action='store', type=int, default=0)
+    parser.add_argument('-dup', '--duplication_chance', help='The probability of losing a species in each species tree',
+                        action='store', type=float, default=0)
+    parser.add_argument('-ndp', '--num_duplications', help='The number of times to try adding a species from the tree',
                         action='store', type=int, default=0)
 
     in_args = parser.parse_args()
@@ -143,10 +159,13 @@ def main():
 
     in_args.branch_stdev = None if not in_args.branch_length else in_args.branch_stdev
     in_args.num_drops = 0 if in_args.drop_chance <= 0 else max(in_args.num_drops, 1)
+    in_args.num_duplications = 0 if in_args.duplication_chance <= 0 else max(in_args.num_duplications, 1)
 
     # Tree Generation #
     generator = TreeGenerator(groups, ntaxa, branch_length=in_args.branch_length, branch_stdev=in_args.branch_stdev,
-                              drop_chance=in_args.drop_chance, num_drops=in_args.num_drops)
+                              drop_chance=in_args.drop_chance, num_drops=in_args.num_drops,
+                              duplication_chance=in_args.duplication_chance,
+                              num_duplications=in_args.num_duplications)
     tree_string = str(generator)
     print(tree_string)
 
