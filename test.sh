218f77483d8c63d295edf9eb1a7654211683c88f
biologyguy <biologyguy@gmail.com>
2017-01-05 17:40:16 -0500

    Activate Travis.ci and Coveralls
    Probably some mistakes, but won't know until I try!

---
+++ test.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+# -*- coding: utf-8 -*-
+
+export PATH="$HOME/miniconda/bin:$PATH"
+FAILURE=0
+
+# Disable py.test cacheprovider because it requires r/w access to the test directory
+#### Pre-tests
+cd /home/travis/build/biologyguy/RD-MCL/rdmcl/tests
+TEST_SCRIPTS='test_rdmcl.py '
+py.test ${TEST_SCRIPTS} --cache-clear -p no:cacheprovider --cov --cov-report= --cov-config ../.coveragerc --durations=10
+if [ $? -ne 0 ]
+then
+    FAILURE=1
+fi
+mv .coverage /home/travis/build/biologyguy/RD-MCL/
+
+
+#### Run Coveralls
+cd /home/travis/build/biologyguy/RD-MCL
+coveralls
+
+exit ${FAILURE}
