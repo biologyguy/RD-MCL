0db5a9eb2aafdfe35dcb0f3694f82832a7a98eda
biologyguy <biologyguy@gmail.com>
2017-01-17 10:41:37 -0500

    Implementation of Markov Clustering algorithm
    This will eventually replace the Popen calls to mcl
    Not yet tested, but seems to be more or less working

--- rdmcl/helpers.py
+++ rdmcl/helpers.py
@@ -5,12 +5,14 @@ import re
 import json
 import logging
 import shutil
-from math import log
+import pandas as pd
+import numpy as np
+from math import log, sqrt
 from time import time
 from copy import copy
 from hashlib import md5
 from multiprocessing import SimpleQueue, Process, Pipe
-from buddysuite.buddy_resources import pretty_time, TempDir
+from buddysuite.buddy_resources import pretty_time, TempDir, SafetyValve
 
 
 class SQLiteBroker(object):
@@ -144,3 +146,71 @@ def bit_score(raw_score):
     bit_lambda = 0.252
     bits = ((bit_lambda * raw_score) - (log(bit_k_value))) / log(2)
     return bits
+
+
+def markov_clustering(data, inflation, edge_sim_threshold=0.):
+    def compare(matrix1, matrix2):
+        dif_mat = matrix1 - matrix2
+        dif_mat **= 2
+        return dif_mat.sum().sum()
+
+    def normalize(matrix):
+        for indx in range(len(matrix)):
+            column = matrix[:, indx]
+            column_sum = column.sum()
+            if column_sum != 0:
+                matrix[:, indx] = column / column_sum
+        return matrix
+
+    def mcl_step(matrix):
+        # Expand
+        matrix = matrix.dot(matrix)
+        # Inflate
+        matrix = matrix ** inflation
+        # Re-normalize
+        matrix = normalize(matrix)
+        return matrix
+
+    def apply_edge_sim_threshold(threshold, matrix):
+        matrix[matrix <= threshold] = 1e-308
+        print(matrix)
+        return matrix
+
+    def df_to_transition_matrix(graph_df):
+        size = (sqrt(8 * len(graph_df) + 1) + 1) / 2
+        if not size.is_integer():
+            raise ValueError("The provided dataframe is not a symmetric graph")
+        score_sum = graph_df.score.sum()
+        graph_df['transition_prob'] = graph_df.score / score_sum
+        size = int(size)
+        tran_mat = np.zeros([size, size])
+        name_order = sorted(list(set(graph_df.seq1.tolist() + graph_df.seq2.tolist())))
+        for indx, row in graph_df.iterrows():
+            seq1 = name_order.index(row.seq1)
+            seq2 = name_order.index(row.seq2)
+            tran_mat[seq1][seq2] = row.score
+            tran_mat[seq2][seq1] = row.score
+        tran_mat = apply_edge_sim_threshold(edge_sim_threshold, tran_mat)
+        tran_mat = normalize(tran_mat)
+        return tran_mat
+
+    valve = SafetyValve(global_reps=10)
+    transition_matrix = df_to_transition_matrix(data)
+    sub_states = [pd.DataFrame(transition_matrix)]
+    counter = 1
+    while True:
+        valve.step()
+        transition_matrix = mcl_step(transition_matrix)
+        sub_states.append(pd.DataFrame(transition_matrix))
+        if compare(sub_states[-2], sub_states[-1]) == 0:
+            break
+        counter += 1
+    return sub_states
+
+
+if __name__ == '__main__':
+    sample_df = pd.read_csv("../workshop/mcl/complete_all_by_all.scores", sep="\t", header=None, index_col=False)
+    sample_df.columns = ["seq1", "seq2", "score"]
+    mcl_steps = markov_clustering(sample_df, 8, 0.6)
+    print(mcl_steps[0])
+    print(mcl_steps[-1])
