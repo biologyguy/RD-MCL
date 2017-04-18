140801b8264599aa5102d4f6aac26003d6087d5e
biologyguy <biologyguy@gmail.com>
2017-04-18 11:49:28 -0400

    Allow gene and species branch lengths to be set independently
    This give much finer control over the shape of the trees generated.

--- evolution_sim/evolution_sim.py
+++ evolution_sim/evolution_sim.py
@@ -53,11 +53,12 @@ def generate_perfect_tree(num_taxa, groups):  # Generates a perfect bipartition 
 
 
 class TreeGenerator:
-    def __init__(self, num_paralogs, num_taxa, seed=None, branch_length=1.0, branch_stdev=None, drop_chance=0.0,
+    def __init__(self, num_paralogs, num_taxa, seed=None, gene_tree_branch_len=1.0, spec_tree_branch_len=1.0,
+                 gene_tree_branch_stdev=None, spec_tree_branch_stdev=None, drop_chance=0.0,
                  num_drops=0, duplication_chance=0.0, num_duplications=0):
         self.num_genes = num_paralogs
         self.num_taxa = num_taxa
-        self.branch_length = branch_length
+        self.branch_length = gene_tree_branch_len
 
         self.rand_gen = Random(seed)  # Random seed for the hashing
         self.drop_chance = drop_chance
@@ -74,8 +75,8 @@ class TreeGenerator:
             self._generate_taxa_name()
         labels = ["%s-GENE_NAME" % self.taxa[x] for x in range(num_taxa)]  # Generates a list of taxa
 
-        self.gene_tree = Tree.randomized(num_paralogs, branch_length=branch_length, branch_stdev=branch_stdev)
-        self.species_tree = Tree.randomized(labels, branch_length=branch_length, branch_stdev=branch_stdev)
+        self.gene_tree = Tree.randomized(num_paralogs, branch_length=gene_tree_branch_len, branch_stdev=gene_tree_branch_stdev)
+        self.species_tree = Tree.randomized(labels, branch_length=spec_tree_branch_len, branch_stdev=spec_tree_branch_stdev)
         self.root = self.gene_tree.clade
 
         self._recursive_build(self.root)  # Assembles the tree
@@ -154,19 +155,19 @@ class TreeGenerator:
         return re.sub("\)n[0-9]*", ")", tree_string)  # Removes strange node IDs that biopython includes
 
 
-def broker_func(queue):
-    sqlite_file = "{0}/{0}.sqlite".format(output_dir)
+def broker_func(queue, sqlite_file):
     connection = sqlite3.connect(sqlite_file)
     cursor = connection.cursor()
     try:
-        cols = [("num_paralogs", "INTEGER"), ("num_taxa", "INTEGER"), ("model", "TEXT"), ("branch", "REAL"), ("stdev", "TEXT"),
-                ("alpha", "REAL"), ("catnum", "INTEGER"), ("drop_chance", "REAL"), ("dropnum", "INTEGER"), ("dup", "REAL"),
-                ("dupnum", "INTEGER"), ("tree", "TEXT"), ("groups_file", "TEXT"), ("alignment", "TEXT")]
+        cols = [("key", "INTEGER PRIMARY KEY"), ("num_paralogs", "INTEGER"), ("num_taxa", "INTEGER"), ("model", "TEXT"),
+                ("gene_branch", "REAL"), ("gene_stdev", "TEXT"), ("species_branch", "REAL"), ("species_stdev", "TEXT"),
+                ("alpha", "REAL"), ("catnum", "INTEGER"), ("drop_chance", "REAL"), ("dropnum", "INTEGER"),
+                ("dup", "REAL"), ("dupnum", "INTEGER"), ("tree", "TEXT"), ("groups_file", "TEXT"),
+                ("alignment", "TEXT")]
 
         cursor.execute("CREATE TABLE data_table (%s)" % ", ".join(["%s %s" % (a, b) for a, b in cols]))
     except sqlite3.OperationalError as err:
-        if "table data_table already exists" not in str(err):
-            raise err
+        pass
 
     while True:
         if not queue.empty():
@@ -200,10 +201,13 @@ def fetch(command):
 
 
 def generate(args):
-    num_paralogs, num_taxa, model_type, branch_length, branch_stdev, alpha, categories, drop_chance, num_drops,\
-        duplication_chance, num_duplications = args
-    generator = TreeGenerator(num_paralogs, num_taxa, branch_length=branch_length, branch_stdev=branch_stdev,
-                              drop_chance=drop_chance, num_drops=num_drops, duplication_chance=duplication_chance,
+    num_paralogs, num_taxa, model_type, gene_tree_branch_len, gene_tree_branch_stdev, spec_tree_branch_len,\
+     spec_tree_branch_stdev, alpha, categories, drop_chance, num_drops, duplication_chance, num_duplications = args
+
+    generator = TreeGenerator(num_paralogs, num_taxa, gene_tree_branch_len=gene_tree_branch_len,
+                              gene_tree_branch_stdev=gene_tree_branch_stdev, spec_tree_branch_len=spec_tree_branch_len,
+                              spec_tree_branch_stdev=spec_tree_branch_stdev, drop_chance=drop_chance,
+                              num_drops=num_drops, duplication_chance=duplication_chance,
                               num_duplications=num_duplications)
 
     tree_string = str(generator)
@@ -225,29 +229,16 @@ def generate(args):
     with open(alignment_file.path, 'r') as aln_data:
         alignment = aln_data.read()
 
-    fields = ["num_paralogs", "num_taxa", "model", "branch", "stdev", "alpha", "catnum", "drop_chance", "dropnum", "dup", "dupnum",
-              "tree", "groups_file", "alignment"]
+    fields = ["num_paralogs", "num_taxa", "model", "gene_branch", "gene_stdev", "species_branch", "species_stdev",
+              "alpha", "catnum", "drop_chance", "dropnum", "dup", "dupnum", "tree", "groups_file", "alignment"]
 
-    values = [num_paralogs, num_taxa, "'%s'" % model_type, branch_length, "'%s'" % branch_stdev, alpha, categories, drop_chance, num_drops,
+    values = [num_paralogs, num_taxa, "'%s'" % model_type, gene_tree_branch_len, "'%s'" % gene_tree_branch_stdev,
+              spec_tree_branch_len, "'%s'" % spec_tree_branch_stdev, alpha, categories, drop_chance, num_drops,
               duplication_chance, num_duplications, "'%s'" % tree_string, "'%s'" % groups_string, "'%s'" % alignment]
     values = [str(value) for value in values]
 
     push("INSERT INTO data_table (%s) VALUES (%s)" % (",".join(fields), ", ".join(values)))
     return
-
-
-def rd_mcl():  # This is broken, because num_runs...
-    print()
-    printer = DynamicPrint()
-    for run in range(num_runs):
-        tmp_file = TempFile()
-        msa = fetch("SELECT (alignment) FROM data_table WHERE runid='{0}'".format(run))
-        tmp_file.write(msa)
-        outdir = "{0}/rdmcl_{1:05d}".format(output_dir, run)
-        printer.write("Processing run {0} of {1}...".format(run, num_runs-1))
-        subprocess.run("../rdmcl/rdmcl.py {0} {1} {2}".format(tmp_file.path, outdir, rdmcl_flags), shell=True,
-                       stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    print()
 
 
 def make_range(lower, upper, step):
@@ -274,30 +265,37 @@ def make_range_from_inargs(_input):
 if __name__ == '__main__':
     # Argument Parsing #
     parser = argparse.ArgumentParser(prog="evolution_sim.py", usage=argparse.SUPPRESS,
-                                     description='A tool for generating orthogroups and running RD-MCL on them.\nUsage:'
+                                     description='A tool for generating orthogroups.\nUsage:'
                                                  '\n./evolution_sim.py seed_file output_dir -nt <#> -ng <#> ...')
     parser.add_argument('seed_file', metavar='seqfile', type=str, help="The seed sequence file (most formats are fine)")
-    parser.add_argument('output_dir', metavar='name', type=str, help="A name for your run")
+    parser.add_argument('-o', '--output', metavar='name', type=str, help="Name your database", default="trees.sqlite")
 
     parser.add_argument('-nt', '--num_taxa', nargs='+', type=int, required=True,
                         help='The number of taxa to be generated. Specify number or range')
     parser.add_argument('-np', '--num_paralogs', nargs='+', type=int, required=True,
                         help='The number of orthogroups to be generated. Specify number or range')
-
     parser.add_argument('-rpl', '--replicates', type=int, default=1,
                         help='Specify how many of each type of tree should be generated')
-    parser.add_argument('-bl', '--branch_length', nargs="+", type=float, default=[1],
+
+    parser.add_argument('-gbl', '--gene_branch_len', nargs="+", type=float, default=[1],
                         help='The average gene tree branch length as substitutions per position')
-    parser.add_argument('-bs', '--branch_stdev', nargs="+", type=float, default=[0.2],
+    parser.add_argument('-gbs', '--gene_branch_stdev', nargs="+", type=float, default=[0.2],
                         help='The standard deviation around the gene tree branch length')
+    parser.add_argument('-sbl', '--species_branch_len', nargs="+", type=float, default=[1],
+                        help='The average species tree branch length as substitutions per position')
+    parser.add_argument('-sbs', '--species_branch_stdev', nargs="+", type=float, default=[0.2],
+                        help='The standard deviation around the species tree branch length')
+
     parser.add_argument('-drp', '--drop_chance', nargs='+', type=float, default=[0],
                         help='The probability of losing a species in each species tree')
     parser.add_argument('-ndr', '--num_drops', nargs='+', type=int, default=[0],
                         help='The number of times to try dropping a species from the tree')
+
     parser.add_argument('-dup', '--duplication_chance', nargs='+', type=float, default=[0],
                         help='The probability of losing a species in each species tree')
     parser.add_argument('-ndp', '--num_duplications', nargs='+', type=int, default=[0],
                         help='The number of times to try adding a species from the tree')
+
     parser.add_argument('-mdl', '--models', nargs='+', default=['WAG'], choices=['WAG', 'JTT', 'LG'],
                         help='What evolutionary model do you want to apply?')
     parser.add_argument('-alp', '--alpha', nargs='+', type=float, default=[2],
@@ -305,17 +303,7 @@ if __name__ == '__main__':
     parser.add_argument('-cat', '--categories', nargs='+', type=int, default=[3],
                         help='The number of gamma categories.')
 
-    parser.add_argument('-skp', '--skip_rdmcl',
-                        help='Don\'t run RD-MCL on the generated data.', action='store_true', default=False)
-    parser.add_argument('-arg', '--rdmcl_args',
-                        help='Arguments to be passed to RD-MCL.',
-                        action='store', default='')
-
     in_args = parser.parse_args()
-
-    call_rdmcl = not in_args.skip_rdmcl
-    output_dir = in_args.output_dir  # Please name your run
-    rdmcl_flags = in_args.rdmcl_args
 
     taxa_range = sorted(in_args.num_taxa)
     taxa_range = taxa_range if len(taxa_range) == 1 else list(range(taxa_range[0], taxa_range[1] + 1))
@@ -323,8 +311,11 @@ if __name__ == '__main__':
     group_range = sorted(in_args.num_paralogs)
     group_range = group_range if len(group_range) == 1 else list(range(group_range[0], group_range[1] + 1))
 
-    branch_lengths = make_range_from_inargs(in_args.branch_length)
-    branch_stdevs = make_range_from_inargs(in_args.branch_stdev)
+    gene_branch_len = make_range_from_inargs(in_args.gene_branch_len)
+    gene_branch_stdev = make_range_from_inargs(in_args.gene_branch_stdev)
+
+    species_branch_len = make_range_from_inargs(in_args.species_branch_len)
+    species_branch_stdev = make_range_from_inargs(in_args.species_branch_stdev)
 
     drop_chances = make_range_from_inargs(in_args.drop_chance)
     if drop_chances[-1] >= 1:
@@ -360,29 +351,27 @@ if __name__ == '__main__':
     for grp in group_range:
         for tax in taxa_range:
             for mdl in models:
-                for br in branch_lengths:
-                    for stdv in branch_stdevs:
-                        for alp in alphas:
-                            for cat in category_range:
-                                for drp in drop_chances:
-                                    for ndr in num_drops_range:
-                                        for dup in duplication_chances:
-                                            for ndp in num_duplications_range:
-                                                arguments.append((grp, tax, mdl, br, stdv, alp, 
-                                                                  cat, drp, ndr, dup, ndp))
+                for gbr in gene_branch_len:
+                    for gstdv in gene_branch_stdev:
+                        for sbr in species_branch_len:
+                            for sstdv in species_branch_stdev:
+                                for alp in alphas:
+                                    for cat in category_range:
+                                        for drp in drop_chances:
+                                            for ndr in num_drops_range:
+                                                for dup in duplication_chances:
+                                                    for ndp in num_duplications_range:
+                                                        arguments.append((grp, tax, mdl, gbr, gstdv, sbr, sstdv,
+                                                                          alp, cat, drp, ndr, dup, ndp))
 
     arguments *= in_args.replicates
     broker_queue = SimpleQueue()
-    broker = Process(target=broker_func, args=[broker_queue])
+    broker = Process(target=broker_func, args=[broker_queue, in_args.output])
     broker.daemon = True
     broker.start()
 
-    os.makedirs(output_dir, exist_ok=True)
-
     run_multicore_function(arguments, generate)
 
-    with open("site_rates_info.txt", "a") as ofile:
-        ofile.write("\n")
     os.remove("site_rates_info.txt")
     os.remove("site_rates.txt")
 
