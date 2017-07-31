6733ddc2fdf0e2dd9459f6f0707e7614188512cf
Steve Bond <biologyguy@gmail.com>
2017-07-31 12:37:54 -0400

    Don't crash if database files are missing
    Just catch the case and write a helpful message while staying in the
     active loop.

--- rdmcl/monitor_dbs.py
+++ rdmcl/monitor_dbs.py
@@ -38,35 +38,38 @@ class Monitor(object):
                 break
 
             split_time = time()
-            with helpers.ExclusiveConnect(self.hbdb_path) as cursor:
-                heartbeat = cursor.execute("SELECT * FROM heartbeat").fetchall()
-            with helpers.ExclusiveConnect(self.wdb_path) as cursor:
-                queue = cursor.execute("SELECT master_id FROM queue").fetchall()
-                processing = cursor.execute("SELECT master_id, worker_id FROM processing").fetchall()
-                complete = cursor.execute("SELECT master_id, worker_id FROM complete").fetchall()
-                waiting = cursor.execute("SELECT * FROM waiting").fetchall()
-            connect_time = round(time() - split_time, 3)
-            master_hb = [hb[2] for hb in heartbeat if hb[1] == "master"]
-            len_mas = len(master_hb)
-            master_hb = 0 if not master_hb else round(time() - (sum(master_hb) / len(master_hb)), 1)
-            worker_hb = [hb[2] for hb in heartbeat if hb[1] == "worker"]
-            len_wor = len(worker_hb)
-            worker_hb = 0 if not worker_hb else round(time() - (sum(worker_hb) / len(worker_hb)), 1)
+            if os.path.isfile(self.hbdb_path) and os.path.isfile(self.wdb_path):
+                with helpers.ExclusiveConnect(self.hbdb_path) as cursor:
+                    heartbeat = cursor.execute("SELECT * FROM heartbeat").fetchall()
+                with helpers.ExclusiveConnect(self.wdb_path) as cursor:
+                    queue = cursor.execute("SELECT master_id FROM queue").fetchall()
+                    processing = cursor.execute("SELECT master_id, worker_id FROM processing").fetchall()
+                    complete = cursor.execute("SELECT master_id, worker_id FROM complete").fetchall()
+                    waiting = cursor.execute("SELECT * FROM waiting").fetchall()
+                connect_time = round(time() - split_time, 3)
+                master_hb = [hb[2] for hb in heartbeat if hb[1] == "master"]
+                len_mas = len(master_hb)
+                master_hb = 0 if not master_hb else round(time() - (sum(master_hb) / len(master_hb)), 1)
+                worker_hb = [hb[2] for hb in heartbeat if hb[1] == "worker"]
+                len_wor = len(worker_hb)
+                worker_hb = 0 if not worker_hb else round(time() - (sum(worker_hb) / len(worker_hb)), 1)
 
-            hash_wait = {}
-            master_wait = {}
-            for seq_hash, master_id in waiting:
-                hash_wait.setdefault(seq_hash, 0)
-                hash_wait[seq_hash] += 1
-                master_wait.setdefault(master_id, 0)
-                master_wait[master_id] += 1
+                hash_wait = {}
+                master_wait = {}
+                for seq_hash, master_id in waiting:
+                    hash_wait.setdefault(seq_hash, 0)
+                    hash_wait[seq_hash] += 1
+                    master_wait.setdefault(master_id, 0)
+                    master_wait[master_id] += 1
 
-            output = [(len_mas, 10), (master_hb, 10), (len_wor, 10), (worker_hb, 10), (len(queue), 10),
-                      (len(processing), 10), (len(complete), 10), (len(hash_wait), 11),
-                      (len(master_wait), 10), (connect_time, 10)]
-            output = [str(x[0]).ljust(x[1]) for x in output]
+                output = [(len_mas, 10), (master_hb, 10), (len_wor, 10), (worker_hb, 10), (len(queue), 10),
+                          (len(processing), 10), (len(complete), 10), (len(hash_wait), 11),
+                          (len(master_wait), 10), (connect_time, 10)]
+                output = [str(x[0]).ljust(x[1]) for x in output]
 
-            printer.write("".join(output))
+                printer.write("".join(output))
+            else:
+                printer.write("Worker databases not detected")
             sleep(0.5)
         return
 
