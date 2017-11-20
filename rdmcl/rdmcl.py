3dbd7392031b58fd54d32d85f7c2c862d0d4e5eb
Steve Bond <biologyguy@gmail.com>
2017-11-20 18:36:22 -0500

    Include installation of HMMER3 in setup script
    Importantly, this includes the generic_forward_backward program, which
     needs to be build from source.
    Also updating the TravisCI yml file to build the proper program (not
     sure if it will work yet...)

--- rdmcl/rdmcl.py
+++ rdmcl/rdmcl.py
@@ -1755,6 +1755,13 @@ class Seqs2Clusters(object):  # ToDo: Logging
         self.large_clusters = OrderedDict()
         self._separate_large_small()
 
+        if os.path.isfile(os.path.join(SCRIPT_PATH, "hmmer", "hmm_fwd_back")):
+            self.hmm_fwd_back_prog = os.path.join(SCRIPT_PATH, "hmmer", "hmm_fwd_back")
+        elif shutil.which("hmm_fwd_back"):
+            self.hmm_fwd_back_prog = shutil.which("hmm_fwd_back")
+        else:
+            raise SystemError("hmm_fwd_back program not found. Please run `rdmcl.py -setup` to fix this.")
+
     def _separate_large_small(self):
         for cluster in self.clusters:
             if len(cluster.seq_ids) < self.min_clust_size:
@@ -1782,8 +1789,7 @@ class Seqs2Clusters(object):  # ToDo: Logging
         self.seqbuddy.write(self.tmp_dir.subfiles[0], out_format="fasta")
         for rec in self.seqbuddy.records:
             hmm_path = os.path.join(hmm_dir, "%s.hmm" % rec.id)
-            # ToDo: generic_fwdback_example needs to be ported into python
-            fwdback_output = Popen("generic_fwdback_example %s %s" % (hmm_path, self.tmp_dir.subfiles[0]),
+            fwdback_output = Popen("%s %s %s" % (self.hmm_fwd_back_prog, hmm_path, self.tmp_dir.subfiles[0]),
                                    shell=True, stdout=PIPE, stderr=PIPE).communicate()[0].decode()
             fwd_scores_df = pd.read_csv(StringIO(fwdback_output), delim_whitespace=True,
                                         header=None, comment="#", index_col=False)
@@ -2418,23 +2424,24 @@ Continue? y/[n] """ % len(sequences)
 
 
 def setup():
-    sys.stdout.write("\033[1mWelcome to RD-MCL!\033[m\nConfirming installation...\n\n")
+    import urllib.request
 
+    sys.stdout.write("\033[1mWelcome to RD-MCL!\033[m\nConfirming installation...\n\n")
     sys.stdout.write("\033[1mChecking for PSIPRED:\033[m ")
     path_install = []
     local_install = []
-    programs = ["seq2mtx", "psipred", "psipass2"]
+    programs = ["psipass2", "psipred", "seq2mtx"]
     for prog in programs:
         if shutil.which(prog):
             path_install.append(prog)
-        elif os.path.isfile("{0}{1}psipred{1}bin{1}{2}".format(SCRIPT_PATH, os.sep, prog)):
+        elif os.path.isfile(os.path.join(SCRIPT_PATH, "psipred", "bin", prog)):
             local_install.append(prog)
-    if path_install == programs:
+    if sorted(list(set(path_install + local_install))) == programs:
         sys.stdout.write("\033[92mFound\033[39m\n")
-        path_install, local_install = True, False
-    elif local_install == programs:
-        sys.stdout.write("\033[92mFound\033[39m\n")
-        path_install, local_install = False, True
+        if local_install:
+            path_install, local_install = False, True
+        else:
+            path_install, local_install = True, False
     else:
         sys.stdout.write("\033[91mMissing\033[39m\n\n")
         if br.ask("Would you like the setup script to try and install PSIPRED? [y]/n:"):
@@ -2442,7 +2449,7 @@ def setup():
                 sys.stdout.write("\033[1mCalling conda...\033[m\n")
                 path_install, local_install = True, False
                 Popen("conda install -y -c biocore psipred", shell=True).wait()
-            elif shutil.which("wget"):
+            else:
                 path_install, local_install = False, True
                 cwd = os.getcwd()
                 tmp_dir = br.TempDir()
@@ -2452,13 +2459,13 @@ def setup():
                 else:
                     version = "linux-64"
                 url = "https://anaconda.org/biocore/psipred/4.01/download/%s/psipred-4.01-1.tar.bz2" % version
-                sys.stdout.write("\n\033[1mDownloading %s binaries from %s\033[m\n\n" % (version, url))
-                Popen("wget %s" % url, shell=True).wait()
+                sys.stdout.write("\n\033[1mDownloading %s binaries from %s\033[m\n" % (version, url))
+                urllib.request.urlretrieve(url, "psipred-4.01-1.tar.bz2")
 
-                sys.stdout.write("\n\033[1mUnpacking...\033[m\n")
+                sys.stdout.write("\033[1mUnpacking...\033[m\n")
                 Popen("tar -xjf psipred-4.01-1.tar.bz2", shell=True).wait()
 
-                sys.stdout.write("\n\033[1mInstalling...\033[m\n")
+                sys.stdout.write("\033[1mInstalling...\033[m\n")
                 if os.path.isdir("%s%spsipred" % (SCRIPT_PATH, os.sep)):
                     shutil.rmtree("%s%spsipred" % (SCRIPT_PATH, os.sep))
                 os.makedirs("%s%spsipred" % (SCRIPT_PATH, os.sep))
@@ -2467,10 +2474,15 @@ def setup():
                 shutil.move("share{0}psipred_4.01{0}data".format(os.sep),
                             "{0}{1}psipred{1}".format(SCRIPT_PATH, os.sep))
                 os.chdir(cwd)
-            else:
-                sys.stdout.write("\033[91mFailed to install PSIPRED.\033[39m\nPlease install the software yourself from"
-                                 " http://bioinfadmin.cs.ucl.ac.uk/downloads/psipred/\n\n")
-                return
+
+        if not shutil.which("psipred") and not os.path.isfile(os.path.join(SCRIPT_PATH, "psipred", "bin", "psipred")):
+            sys.stdout.write("\033[91mRD-MCL depends on PSIPRED, and it is not installed correctly.\033[39m\n"
+                             "Please see instructions at"
+                             " github.com/biologyguy/RD-MCL/wiki/Installation-Guide#psipred\n\n")
+            return
+        else:
+            sys.stdout.write("\033[92mPSIPRED binary installed\033[39m\n\n")
+
     # Confirm all psipred weight files are in the rdmcl directory
     weight_files = ["weights.dat", "weights.dat2", "weights.dat3", "weights_p2.dat",
                     "weights_s.dat", "weights_s.dat2", "weights_s.dat3"]
@@ -2508,8 +2520,63 @@ def setup():
                 print(error_msg.format(next_file, data_dir))
                 return
 
+    sys.stdout.write("\033[1mChecking for HMMER3 programs:\033[m\n")
+    not_installed = []
+    for program in ["hmmbuild", "hmm_fwd_back"]:
+        if not shutil.which(program) and not os.path.isfile(os.path.join(SCRIPT_PATH, "hmmer", program)):
+            sys.stdout.write("\t\033[1m%s: \033[91mMissing\033[39m\n" % program)
+            not_installed.append(program)
+        else:
+            sys.stdout.write("\t\033[1m%s: \033[92mFound\033[39m\n" % program)
+    if not_installed:
+        if br.ask("Would you like the setup script to try and install missing HMMER3 programs? [y]/n:"):
+            cwd = os.getcwd()
+            temp_dir = br.TempDir()
+            os.chdir(temp_dir.path)
+            url = "http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2.tar.gz"
+            sys.stdout.write("\n\033[1mDownloading hmmer-3.1b2.tar.gz\033[m\n")
+            urllib.request.urlretrieve(url, "hmmer-3.1b2.tar.gz")
+
+            sys.stdout.write("\033[1mUnpacking...\033[m\n")
+            Popen("tar -xzf hmmer-3.1b2.tar.gz", shell=True).wait()
+            if not os.path.isdir("hmmer-3.1b2"):
+                sys.stdout.write("\033[91mFailed to download HMMER3.\033[39m\nPlease see instructions at"
+                                 " github.com/biologyguy/RD-MCL/wiki/Installation-Guide#hmmer3\n\n")
+                return
+            os.chdir("hmmer-3.1b2")
+
+            sys.stdout.write("\033[1mInstalling...\033[m\n")
+            Popen("./configure; make;", shell=True).wait()
+            os.chdir("src")
+            with open("generic_fwdback.c", "r") as ifile:
+                modified = re.sub(r'(p7_gmx_Dump\(stdout, fwd, p7_DEFAULT\);)', '', ifile.read())
+
+            with open("generic_fwdback.c", "w") as ofile:
+                ofile.write(modified)
+
+            Popen("make generic_fwdback_example", shell=True).wait()
+
+            os.makedirs(os.path.join(SCRIPT_PATH, "hmmer"), exist_ok=True)
+            if "hmm_fwd_back" in not_installed:
+                shutil.move("generic_fwdback_example", os.path.join(SCRIPT_PATH, "hmmer", "hmm_fwd_back"))
+            if "hmmbuild" in not_installed:
+                shutil.move("hmmbuild", os.path.join(SCRIPT_PATH, "hmmer", "hmmbuild"))
+            os.chdir(cwd)
+
+        else:
+            sys.stdout.write("\033[91mRD-MCL depends on HMMER3.\033[39m\nPlease see instructions at"
+                             " github.com/biologyguy/RD-MCL/wiki/Installation-Guide#hmmer3\n\n")
+            return
+
+        for program in ["hmmbuild", "hmm_fwd_back"]:
+            if not shutil.which(program) and not os.path.isfile(os.path.join(SCRIPT_PATH, "hmmer", program)):
+                sys.stdout.write("\033[91mFailed to install HMMER3 programs.\033[39m\nPlease see instructions at"
+                                 " github.com/biologyguy/RD-MCL/wiki/Installation-Guide#hmmer3\n\n")
+                return
+        else:
+            sys.stdout.write("\033[92mHMMER3 binaries installed\033[39m\n\n")
     open(os.path.join(SCRIPT_PATH, "config.ini"), "w").close()
-    sys.stdout.write("\n\033[1mSuccess! You're all set.\033[m\n")
+    sys.stdout.write("\033[1mSuccess! You're all set.\033[m\n")
     return
 
 
