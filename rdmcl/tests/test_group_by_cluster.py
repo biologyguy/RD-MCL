184ba7baeb55352796c13c49099f5e03c057db1d
biologyguy <biologyguy@gmail.com>
2017-12-21 15:34:33 -0500

    Add unit tests for group_by_cluster

---
+++ rdmcl/tests/test_group_by_cluster.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pytest
+from .. import group_by_cluster
+from buddysuite import SeqBuddy as Sb
+from buddysuite import AlignBuddy as Alb
+from buddysuite import buddy_resources as br
+import os
+import sys
+
+
+def test_make_msa(hf, monkeypatch):
+    seqbuddy = hf.get_data("cteno_panxs")
+    seqbuddy.records = seqbuddy.records[:2]
+    alb_obj = group_by_cluster.make_msa(seqbuddy, "clustalo")
+    assert type(alb_obj) == Alb.AlignBuddy
+    assert str(alb_obj) == """\
+>Bab-PanxαA Be_abyssicola|m.8 and m.21|ML036514|937+ 2.
+MLLLGSLGTIKNLSIFKDLSLDDWLDQMNRTFMFLLLCFMGTIVAVSQYTGKNISCNGFE
+KFSDDFSQDYCWTQGLYTIKEAYDLPESQIPYPGIIPENVPACREHSLKNGGKIICPPPE
+EIKPLTRARHLWYQWIPFYFWVIAPVFYLPYMFVKRMGLDRMKPLLKIMSDYYHCTTETP
+SEEIIVKCADWVYNSIVDRLSEGSSWTSWRNRHGLGLAVLFSKLMYLGGSILVMMVTTLM
+FQVGDFKTYGIEWLKQFPSDENYTTSVKHKLFPKMVACEIKRWGPSGLEEENGMCVLAPN
+VIYQYIFLIMWFALAITICTNFFNIFFWVFKLTATRYTYSKLVATGHFSHKHPGWKFMYY
+RIGTSGRVLLNIVAQNTNPIIFGAIMEKLTPSVIKHLRIGHVPGEYLTDPA
+>Bab-PanxαB Be_abyssicola|m.19|ML47742|1063 2.
+--MLDILSKFKGVTPFKGITIDDGWDQLNRSFMFVLLVVMGTTVTVRQYTGSVISCDGFK
+KFGSTFAEDYCWTQGLYTVLEGYDQPSYNIPYPGLLPDELPACTPVKLKDGTRLKCPDAD
+QLMSPTRISHLWYQWVPFYFWLAAAAFFMPYLLYKNFGMGDIKPLVRLLHNPVESDQ--E
+LKKMTDKAATWLFYKFDLYMSEQSLVASLTRKHGLGLSMVFVKILYAAVSFCCFILTAEM
+FSIGDFKTYGSKWIKKMRYEDTLATEEKDKLFPKMVACEVKRWGASGIEEEQGMCVLAPN
+VINQYLFLILWFCLVFVMICNIVSIFVSLIKLLFTYGSYRRLLST-AFLRDDSAIKHMYF
+NVGSSGRLILHVLANNTAPRVFEDILLTLAPKLIQRKLRGNGKAV------
+"""
+
+    seqbuddy.records = [seqbuddy.records[0]]
+    alb_obj = group_by_cluster.make_msa(seqbuddy, "clustalo")
+    assert type(alb_obj) == Alb.AlignBuddy
+    assert str(alb_obj) == """\
+>Bab-PanxαA Be_abyssicola|m.8 and m.21|ML036514|937+ 2.
+MLLLGSLGTIKNLSIFKDLSLDDWLDQMNRTFMFLLLCFMGTIVAVSQYTGKNISCNGFE
+KFSDDFSQDYCWTQGLYTIKEAYDLPESQIPYPGIIPENVPACREHSLKNGGKIICPPPE
+EIKPLTRARHLWYQWIPFYFWVIAPVFYLPYMFVKRMGLDRMKPLLKIMSDYYHCTTETP
+SEEIIVKCADWVYNSIVDRLSEGSSWTSWRNRHGLGLAVLFSKLMYLGGSILVMMVTTLM
+FQVGDFKTYGIEWLKQFPSDENYTTSVKHKLFPKMVACEIKRWGPSGLEEENGMCVLAPN
+VIYQYIFLIMWFALAITICTNFFNIFFWVFKLTATRYTYSKLVATGHFSHKHPGWKFMYY
+RIGTSGRVLLNIVAQNTNPIIFGAIMEKLTPSVIKHLRIGHVPGEYLTDPA
+"""
+
+    # Don't modify if any sequence is reduced to nothing
+    align = Alb.AlignBuddy("""\
+>A
+MSTGTC-------
+>B
+M---TC-------
+>C
+M---TC---AILP
+>D
+-STP---YWAILP
+""", in_format="fasta")
+
+    seqbuddy = Sb.SeqBuddy(Alb.make_copy(align).records(), in_format="fasta")
+    seqbuddy = Sb.clean_seq(seqbuddy)
+
+    monkeypatch.setattr(Alb, "generate_msa", lambda *_, **__: align)
+    alb_obj = group_by_cluster.make_msa(seqbuddy, "clustalo", trimal=[0.3])
+    assert str(alb_obj) == str(align)
+
+    # Don't modify if average sequence length is reduced by more than half
+    align = Alb.AlignBuddy("""\
+>A
+MSTGTC-------
+>B
+M---TC-------
+>C
+M---TC---AILP
+>D
+-STPTC-YWAILP
+""", in_format="fasta")
+
+    seqbuddy = Sb.SeqBuddy(Alb.make_copy(align).records(), in_format="fasta")
+    seqbuddy = Sb.clean_seq(seqbuddy)
+
+    monkeypatch.setattr(Alb, "generate_msa", lambda *_, **__: align)
+    alb_obj = group_by_cluster.make_msa(seqbuddy, "clustalo", trimal=[0.3])
+    assert str(alb_obj) == str(align)
+
+    # Remove some gaps
+    alb_obj = group_by_cluster.make_msa(seqbuddy, "clustalo", trimal=[0.3, 0.55])
+    assert str(alb_obj) == """\
+>A
+MSTGTC----
+>B
+M---TC----
+>C
+M---TCAILP
+>D
+-STPTCAILP
+"""
+
+
+def test_argparse_init(monkeypatch, hf):
+    argv = ['rdmcl.py', os.path.join(hf.resource_path, "final_clusters.txt"),
+            os.path.join(hf.resource_path, "Cteno_pannexins.fa")]
+    monkeypatch.setattr(sys, "argv", argv)
+    temp_in_args = group_by_cluster.argparse_init()
+    assert temp_in_args.mode == "list"
+    assert temp_in_args.aligner == "clustalo"
+    assert temp_in_args.trimal == ["gappyout", 0.5, 0.75, 0.9, 0.95, "clean"]
+
+
+"""
+parser = argparse.ArgumentParser(prog="group_by_cluster", formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                 description="")
+# Positional
+parser.add_argument("clusters", action="store", help="Path to clusters file")
+parser.add_argument("sequence_file", action="store", help="Path to original sequence file")
+parser.add_argument("mode", action="store", nargs="?", default="list",
+                        help="Choose the output type [list, seqs, sequences, aln, alignment, con, consensus]")
+# Optional commands
+parser.add_argument("--aligner", "-a", action="store", default="clustalo", metavar="",
+                          help="Specify a multiple sequence alignment program")
+parser.add_argument("--groups", "-g", action="append", nargs="+", metavar="group",
+                          help="List the specific groups to process")
+parser.add_argument("--max_size", "-max", action="store", type=int, metavar="",
+                          help="Max cluster size to process")
+parser.add_argument("--min_size", "-min", action="store", type=int, metavar="",
+                          help="Min cluster size to process")
+parser.add_argument("--trimal", "-trm", action="append", nargs="*", metavar="param",
+                          help="Specify trimal parameters",
+                          default=["gappyout", 0.5, 0.75, 0.9, 0.95, "clean"])
+parser.add_argument("--write", "-w", action="store", metavar="", help="Specify directory to write file(s)")
+# Misc
+misc = parser.add_argument_group(title="\033[1mMisc options\033[m")
+misc.add_argument('-h', '--help', action="help", help="Show this help message and exit")
+
+# This is to allow py.test to work with its own flags
+in_args = parser.parse_args([])
+"""
+
+
+def test_main_list(monkeypatch, hf, capsys):
+    argv = ['rdmcl.py', os.path.join(hf.resource_path, "final_clusters.txt"),
+            os.path.join(hf.resource_path, "Cteno_pannexins.fa"), "list"]
+    monkeypatch.setattr(sys, "argv", argv)
+    group_by_cluster.main()
+    out, err = capsys.readouterr()
+    assert hf.string2hash(out) == "dffc4d7bea1917711bf01132a42f2891", print(out)
+
+
+def test_main_seqs(monkeypatch, hf, capsys):
+    argv = ['rdmcl.py', os.path.join(hf.resource_path, "final_clusters.txt"),
+            os.path.join(hf.resource_path, "Cteno_pannexins.fa"), "seqs"]
+    monkeypatch.setattr(sys, "argv", argv)
+    group_by_cluster.main()
+    out, err = capsys.readouterr()
+    assert hf.string2hash(out) == "732ff95ca03b8d56f94318faaf746b05", print(out)
+
+
+def test_main_aln(monkeypatch, hf, capsys):
+    argv = ['rdmcl.py', os.path.join(hf.resource_path, "final_clusters.txt"),
+            os.path.join(hf.resource_path, "Cteno_pannexins.fa"), "aln"]
+    monkeypatch.setattr(sys, "argv", argv)
+    group_by_cluster.main()
+    out, err = capsys.readouterr()
+    assert hf.string2hash(out) == "b4b7dae5b4f81db7e2ea26417f85a054", print(out)
+
+
+def test_main_cons(monkeypatch, hf, capsys):
+    argv = ['rdmcl.py', os.path.join(hf.resource_path, "final_clusters.txt"),
+            os.path.join(hf.resource_path, "Cteno_pannexins.fa"), "cons"]
+    monkeypatch.setattr(sys, "argv", argv)
+    group_by_cluster.main()
+    out, err = capsys.readouterr()
+    assert hf.string2hash(out) == "564054d66d8a8927ff442dbc2918d943", print(out)
+
+
+def test_main_select_groups(monkeypatch, hf, capsys):
+    argv = ['rdmcl.py', os.path.join(hf.resource_path, "final_clusters.txt"),
+            os.path.join(hf.resource_path, "Cteno_pannexins.fa"), "-g", "group_0_1", "group_0_3", "group_0_0"]
+    monkeypatch.setattr(sys, "argv", argv)
+    group_by_cluster.main()
+    out, err = capsys.readouterr()
+    assert hf.string2hash(out) == "9a03d8619d686cc553f09138635c8685", print(out)
+
+
+def test_main_min_max(monkeypatch, hf, capsys):
+    argv = ['rdmcl.py', os.path.join(hf.resource_path, "final_clusters.txt"),
+            os.path.join(hf.resource_path, "Cteno_pannexins.fa"), "-min", "5", "-max", "7"]
+    monkeypatch.setattr(sys, "argv", argv)
+    group_by_cluster.main()
+    out, err = capsys.readouterr()
+    assert hf.string2hash(out) == "c15356cb75c752a1f1b4db69de58114f", print(out)
+
+
+def test_main_write(monkeypatch, hf, capsys):
+    tmp_dir = br.TempDir()
+    argv = ['rdmcl.py', os.path.join(hf.resource_path, "final_clusters.txt"),
+            os.path.join(hf.resource_path, "Cteno_pannexins.fa"), "-w", tmp_dir.path]
+    monkeypatch.setattr(sys, "argv", argv)
+    group_by_cluster.main()
+    root, dirs, files = next(os.walk(tmp_dir.path))
+    assert sorted(files) == ['group_0_0.txt', 'group_0_1.txt', 'group_0_18.txt', 'group_0_19.txt', 'group_0_2.txt',
+                             'group_0_20.txt', 'group_0_23.txt', 'group_0_26.txt', 'group_0_3.txt', 'group_0_30.txt',
+                             'group_0_5.txt', 'group_0_6.txt', 'group_0_7.txt'], print(sorted(files))
+    out, err = capsys.readouterr()
+    assert out + err == ""
+
+
+def test_main_errors(monkeypatch, hf, capsys):
+    argv = ['rdmcl.py', os.path.join(hf.resource_path, "final_clusters.txt"),
+            os.path.join(hf.resource_path, "Cteno_pannexins.fa"), "FOOBAR"]
+    monkeypatch.setattr(sys, "argv", argv)
+    with pytest.raises(SystemExit):
+        group_by_cluster.main()
+    out, err = capsys.readouterr()
+    assert 'Unrecognized mode, please select from ["seqs", "aln", "con", "list"].' in err
+
+    argv = ['rdmcl.py', os.path.join(hf.resource_path, "final_clusters.txt"),
+            os.path.join(hf.resource_path, "Cteno_pannexins.fa"), "aln"]
+    monkeypatch.setattr(sys, "argv", argv)
+
+    def kill1(*_):
+        raise AttributeError("mock msa AttributeError fail")
+
+    monkeypatch.setattr(group_by_cluster, "make_msa", kill1)
+    with pytest.raises(SystemExit):
+        group_by_cluster.main()
+    out, err = capsys.readouterr()
+    assert "mock msa AttributeError fail" in out
+
+    def kill2(*_):
+        raise SystemError("mock msa SystemError fail")
+
+    monkeypatch.setattr(group_by_cluster, "make_msa", kill2)
+    with pytest.raises(SystemExit):
+        group_by_cluster.main()
+    out, err = capsys.readouterr()
+    assert "mock msa SystemError fail" in out
