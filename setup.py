db13c3f5511a662c412fef52ef1cd1008c1d4ceb
biologyguy <biologyguy@gmail.com>
2018-01-17 14:12:30 -0500

    Add merge_orthogroups to setup file

--- setup.py
+++ setup.py
@@ -58,7 +58,8 @@ ENTRY_POINTS = {
         'monitor_dbs = rdmcl.monitor_dbs:main',
         'group_by_cluster = rdmcl.group_by_cluster:main',
         'compare_homolog_groups = rdmcl.compare_homolog_groups:main',
-        'homolog_tree_builder = rdmcl.homolog_tree_builder:main'
+        'homolog_tree_builder = rdmcl.homolog_tree_builder:main',
+        'merge_orthogroups = rdmcl.merge_orthogroups:main'
     ]
 }
 
